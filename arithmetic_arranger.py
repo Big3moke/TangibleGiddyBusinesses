5769fdc9817e6542d580988cbc41d89f2a67441e
p_michael <big3moke@gmail.com>
2020-08-30 18:12:58 -0400

    attempt to solve it

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,20 @@
+
+
 def arithmetic_arranger(problems):
 
+    for numbers in problems:
+        if numbers.find('+') != -1:
+            location = numbers.find('+')
+            distance = (5-location)*" "
+            print(distance, numbers[:location],'\n', numbers[location:], '\n-----','\n\n\n\n')
+        elif numbers.find('-') != -1:
+            location =  numbers.find('-')
+            distance = (5-location)*" "
+            print(distance, numbers[:location],'\n', numbers[location:], '\n-----','\n\n\n\n')
+        elif numbers.find('*') != 1:
+            print("Error: Operator must be '+' or '-'.")
+        elif numbers.find('/') != 1:
+            print("Error: Operator must be '+' or '-'.")
 
-    return arranged_problems
+
+    # return arranged_problems
